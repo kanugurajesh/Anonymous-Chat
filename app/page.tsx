b84c354debfa0c79a5af0cf9b13e65765accd6e1
kanugurajesh <kanugurajesh3@gmail.com>
2024-02-07 20:13:03 +0530

    Adding css to the chat

--- app/page.tsx
+++ app/page.tsx
@@ -26,6 +26,7 @@ export default function Home() {
 
   const handleAddChat = async () => {
     dispatch(addChat({ message: input, image: image }));
+
     const response = await fetch("/api/chat", {
       method: "POST",
       headers: {
@@ -33,6 +34,11 @@ export default function Home() {
       },
       body: JSON.stringify({ message: input, image: image }),
     });
+
+    const data = await response.json();
+    if (data.error) {
+      toast.error(data.error);
+    }
   };
 
   const loadingToast = (loading: boolean) => {
@@ -59,41 +65,49 @@ export default function Home() {
   }, []);
 
   return (
-    <main>
+    <main className="flex flex-col gap-2 p-10">
       <Toaster />
-      <h1>Chat</h1>
+      <h1 className="text-center font-black text-3xl">Chaty</h1>
       <ul>
         {/* @ts-ignore */}
         {chat.map((chat: Chat, index: number) => (
-          <li key={index}>
-            <p>{chat.message}</p>
-            {chat.image && (
-              <Image src={chat.image} alt="image" width={150} height={150} />
-            )}
+          <li key={index} className="mb-5">
+            <div className="flex flex-col gap-3 justify-center">
+              {chat.image && (
+                <Image src={chat.image} alt="image" width={100} height={100} className="float-left"/>
+              )}
+              <p className="bg-black text-white mr-auto p-2 rounded-sm">{chat.message}</p>
+            </div>
           </li>
         ))}
       </ul>
-
-      <UploadButton
-        endpoint="imageUploader"
-        // @ts-ignore
-        onBeforeUploadBegin={(file: File) => {
-          loadingToast(true);
-        }}
-        onClientUploadComplete={(res: any) => {
-          // Do something with the response
-          toast.dismiss();
-          toast.success("Upload complete!");
-          setImage(res[0].url);
-        }}
-        onUploadError={(error: Error) => {
-          // Do something with the error.
-          toast.error(`ERROR! ${error.message}`);
-          loadingToast(false);
-        }}
-      />
-      <input type="text" onChange={(e) => handleInput(e)} />
-      <button onClick={() => handleAddChat()}>Add</button>
+      <div className="flex items-center gap-2">
+        <input
+          type="text"
+          onChange={(e) => handleInput(e)}
+          className="border-2 border-black h-10 w-1/2 p-2 rounded-sm"
+        />
+        <UploadButton
+        className="mt-6"
+          endpoint="imageUploader"
+          // @ts-ignore
+          onBeforeUploadBegin={(file: File) => {
+            loadingToast(true);
+          }}
+          onClientUploadComplete={(res: any) => {
+            // Do something with the response
+            toast.dismiss();
+            toast.success("Upload complete!");
+            setImage(res[0].url);
+          }}
+          onUploadError={(error: Error) => {
+            // Do something with the error.
+            toast.error(`ERROR! ${error.message}`);
+            loadingToast(false);
+          }}
+        />
+        <button className="bg-black text-white px-6 py-2 rounded-sm hover:bg-white hover:border-2 hover:border-black hover:text-black font-bold transition ease-in-out duration-200" onClick={() => handleAddChat()}>Add</button>
+      </div>
     </main>
   );
 }
