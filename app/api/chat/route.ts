b02f190061e06e3d4331bc45f8c449af98d14e6d
kanugurajesh <kanugurajesh3@gmail.com>
2024-02-07 22:33:09 +0530

    changing the schema

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -4,29 +4,39 @@ import chatModel from "@/models/chatModel";
 
 connect();
 
-export async function POST(request:NextRequest) {
-    try {
-        const reqBody = await request.json();
-        const { message, image } = reqBody;
-        const chat = new chatModel({
-            message,
-            image
-        });
-        await chat.save();
-        return NextResponse.json({message: "Chat saved successfully"}, {status: 201});
-    } catch (error) {
-        return NextResponse.json({message: "There was an error saving the chat"}, {status: 500});
-    }
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = await request.json();
+    const { name, message, image } = reqBody;
+    const chat = new chatModel({
+      name,
+      message,
+      image,
+    });
+    await chat.save();
+    return NextResponse.json(
+      { message: "Chat saved successfully" },
+      { status: 201 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { message: "There was an error saving the chat" },
+      { status: 500 }
+    );
+  }
 }
 
 export async function GET() {
-    // return all the chats
-    try {
-        // fetching all the chats from the database
-        const chats = await chatModel.find();
-        // returning the chats
-        return NextResponse.json(chats, {status: 200});   
-    } catch (error) {
-        return NextResponse.json({message: "There was an error fetching the chats"}, {status: 500});
-    }
+  // return all the chats
+  try {
+    // fetching all the chats from the database
+    const chats = await chatModel.find();
+    // returning the chats
+    return NextResponse.json(chats, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { message: "There was an error fetching the chats" },
+      { status: 500 }
+    );
+  }
 }
--- models/chatModel.ts
+++ models/chatModel.ts
@@ -1,14 +1,18 @@
 import mongoose from "mongoose";
 
 const chatSchema = new mongoose.Schema({
-    message: {
-        type: String,
-        default: null
-    },
-    image: {
-        type: String,
-        default: null
-    }
+  name: {
+    type: String,
+    default: "user",
+  },
+  message: {
+    type: String,
+    default: null,
+  },
+  image: {
+    type: String,
+    default: null,
+  },
 });
 
 const Chat = mongoose.models.Chat || mongoose.model("Chat", chatSchema);
