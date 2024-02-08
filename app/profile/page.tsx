e8e7dd6d822731954d58c7eddbfee821a9bc21ab
kanugurajesh <kanugurajesh3@gmail.com>
2024-02-08 17:13:12 +0530

    Adding the changes in the profile page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,4 +1,6 @@
 'use client';
+
+import { useState } from 'react';
 import { useAppDispatch, useAppSelector } from '@/helper/hook';
 import { addProfile, clearProfile } from '@/features/profile/profileSlice';
 import { addName, clearName } from '@/features/name/nameSlice';
@@ -8,12 +10,15 @@ import toast, { Toaster } from 'react-hot-toast';
 import Image from 'next/image';
 
 export default function Profile() {
+  const [input, setInput] = useState<string>('');
+  const [image, setImage] = useState<string>('');
+
   const profile = useAppSelector((state) => state.profile.profile);
   const name = useAppSelector((state) => state.name.name);
 
   const dispatch = useAppDispatch();
   return (
-    <main className='flex flex-col justify-center items-center h-screen'>
+    <main className='flex flex-col justify-center items-center h-screen gap-4'>
       <Toaster />
       <Link
         href='/'
@@ -28,29 +33,79 @@ export default function Profile() {
         Contact Us
       </Link>
       <h1 className='font-black text-xl'>Set Profile</h1>
-      {profile && (
-        <Image src={profile} alt='profile' width={200} height={200} />
+      {(profile || image) && (
+        <Image
+          src={profile || image}
+          alt='profile'
+          width={100}
+          height={100}
+          className='bg-cover '
+        />
       )}
-      {name && <h2 className='font-black text-xl'>{name}</h2>}
+      {(name || input) && (
+        <h2 className='font-black text-xl'>{name || input}</h2>
+      )}
       {/* @ts-ignore */}
       <UploadButton
         className='mt-6'
         endpoint='imageUploader'
         // @ts-ignore
         onBeforeUploadBegin={(file: File) => {
-          toast.loading('Uploading...');
+          toast.loading(`Uploading... ${file.name}`);
         }}
         onClientUploadComplete={(res: any) => {
-          dispatch(addProfile(res[0].url));
+          setImage(res[0].url);
           toast.dismiss();
           toast.success('Profile picture uploaded');
         }}
         onUploadError={(error: Error) => {
           // Do something with the error.
-          toast.error(`ERROR! ${error.message}`);
+          toast.dismiss();
           toast.error(`ERROR! ${error.message}`);
         }}
       />
+      <div className='flex gap-2 justify-center items-center'>
+        <input
+          type='text'
+          onChange={(e) => setInput(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              if (input === '' && image === '') {
+                toast.error('Please set profile and name');
+                return;
+              }
+              if (input) {
+                dispatch(addName(input));
+                toast.success('Name set');
+              }
+              if (image) {
+                dispatch(addProfile(image));
+                toast.success('Profile picture set');
+              }
+            }
+          }}
+          className='border-2 border-black h-10 max-x-[100px] p-2 rounded-sm'
+        />
+        <button
+          onClick={() => {
+            if (input === '' && image === '') {
+              toast.error('Please set profile and name');
+              return;
+            }
+            if (input) {
+              dispatch(addName(input));
+              toast.success('Name set');
+            }
+            if (image) {
+              dispatch(addProfile(image));
+              toast.success('Profile picture set');
+            }
+          }}
+          className='bg-black text-white font-bold p-2 rounded-md hover:bg-white hover:text-black hover:border-2 hover:border-black transition duration-200 ease-in-out outline-none outline-0'
+        >
+          Submit
+        </button>
+      </div>
     </main>
   );
 }
